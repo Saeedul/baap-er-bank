e300f35e9127df5060e33e84773670ab77de49f0
Saeedul <karimsaeedul476@gmail.com>
2023-05-25 16:33:02 +0600

    added more

--- login.js
+++ login.js
@@ -13,9 +13,9 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     // DANGER: DO NOT VERIFY email, password on the client side
     // step: 4 Verify email and password and check whether valid user or not
     if (email === 'sontan@baap.com' && password === 'secret') {
-        console.log('valid user');
+        window.location.href = 'bank.html'
     }
     else {
-        console.log('invalid user');
+        alert('invalid credentials');
     }
 })

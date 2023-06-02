a97add0d1458b08ab237f97d75a1f93f7cb11f8b
Saeedul <karimsaeedul476@gmail.com>
2023-06-03 00:27:44 +0600

    finalized code

--- withdraw.js
+++ withdraw.js
@@ -3,7 +3,11 @@
 // 2-5) also make sure to convert the input into a number by using parseFloat
 // 3) Get previous withdraw total
 // 4) calculate total withdraw amount
-// 4-5) set 
+// 4-5) set total withdraw amount
+// 5) get the previous balance total
+// 6) calculate new balance total
+// 6.5 set the new balance total
+// 7) clear the input field
 
 //step 1:
 document.getElementById('btn-withdraw').addEventListener('click', function () {
@@ -16,11 +20,20 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-    console.log(previousWithdrawTotal);
 
     //step 4:
     const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     //step 5:
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+    //step 6:
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    //step 7:
+    withdrawField.value = '';
 })

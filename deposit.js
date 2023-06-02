c39c022a1efb2180a7cbe302cb0063b35cf105a5
Saeedul <karimsaeedul476@gmail.com>
2023-06-02 17:10:41 +0600

    added more

---
+++ deposit.js
@@ -0,0 +1,5 @@
+// step 1: add event listener to the deposit button
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    // step 2: get the deposit input field
+    const depositField = document.getElementById('deposit-field')
+})
